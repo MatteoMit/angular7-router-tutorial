8c9e39b4abdf42e68c0d14670d03e9de46db889b
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-12-01 11:23:41 +0100

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { HomeComponent } from './app/components/home';
+import { AboutComponent } from './app/components/about';
+import { DashboardComponent } from './app/components/dashboard';
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
